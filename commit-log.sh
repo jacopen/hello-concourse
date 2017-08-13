320462c12767cd572dd9327cb81b1980c4cc5c7d
Kazuto Kusama <jacopen@gmail.com>
2017-08-13 23:48:59 +0900

    update

--- commit-log.sh
+++ commit-log.sh
@@ -4,8 +4,8 @@ git clone result updated-result
 cd updated-result/
 mv -f ../out/* ./
 
-git config --global user.email "makingx@gmail.com"
-git config --global user.name "Toshiaki Maki"
+git config --global user.email "jacopen@gmail.com"
+git config --global user.name "Kazuto Kusama"
 
 git add -A
 git commit -m "Update result log"
